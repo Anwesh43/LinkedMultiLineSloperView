d26e8921685c22ed317869e0b6368b7f25bae73a
Anwesh43 <anweshthecool0@gmail.com>
2020-08-03 03:01:17 +0530

    adding static create method

--- multilineslopperview/src/main/java/com/anwesh/uiprojects/multilineslopperview/MultiLineSlopperView.kt
+++ multilineslopperview/src/main/java/com/anwesh/uiprojects/multilineslopperview/MultiLineSlopperView.kt
@@ -212,4 +212,13 @@ class MultiLineSlopperView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : MultiLineSlopperView {
+            val view : MultiLineSlopperView = MultiLineSlopperView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
