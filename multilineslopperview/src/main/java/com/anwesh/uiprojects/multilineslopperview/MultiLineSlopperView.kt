7bc70a44318939e6799e0cf2f886bbdbf0c1c083
Anwesh43 <anweshthecool0@gmail.com>
2020-08-03 02:59:18 +0530

    adding Renderer class

--- multilineslopperview/src/main/java/com/anwesh/uiprojects/multilineslopperview/MultiLineSlopperView.kt
+++ multilineslopperview/src/main/java/com/anwesh/uiprojects/multilineslopperview/MultiLineSlopperView.kt
@@ -187,4 +187,27 @@ class MultiLineSlopperView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : MultiLineSlopperView) {
+
+        private val animator : Animator = Animator(view)
+        private val mls : MultiLineSlopper = MultiLineSlopper(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            mls.draw(canvas, paint)
+            animator.animate {
+                mls.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            mls.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
