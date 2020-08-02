931777257d28419146cf5b8849823d8056863525
Anwesh43 <anweshthecool0@gmail.com>
2020-08-03 03:04:05 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmultilinesloperview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultilinesloperview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.anwesh.uiprojects.linkedmultilinesloperview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.multilineslopperview.MultiLineSlopperView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiLineSlopperView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
